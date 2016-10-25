3fb99f52b54641f85aea3191269e464d0b77dda9
valontum <valon.xhafa@tum.de>
2016-10-25 21:19:15 +0200

    ukry

---
+++ HelloWorld/src/HelloUser.java
@@ -0,0 +1,12 @@
+
+public class HelloUser {
+	
+	public String userName;
+	
+	public void greetUser()
+	{
+		System.out.println("Hello "+userName);
+		
+	}
+
+}
--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -12,7 +14,18 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+			
+
+
+		Scanner in = new Scanner(System.in);
+
+		HelloUser user= new HelloUser();
+		
 		System.out.println("Hello ASE2016 - how are you? :)");
+		System.out.print("Please enter your name: ");
+		user.userName=in.next();
+		user.greetUser();
+		
 	}
 
 }
